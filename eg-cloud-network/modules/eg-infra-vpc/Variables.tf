506c9484b637772fc5a463f2e8b4ca4484a9914a
SankajRam <44740469+SankajRam@users.noreply.github.com>
2021-12-01 21:37:36 +0530

    Update Variables.tf

--- eg-cloud-network/modules/eg-infra-vpc/Variables.tf
+++ eg-cloud-network/modules/eg-infra-vpc/Variables.tf
@@ -1,16 +1,13 @@
-variable "vpc_cidr"
-{
-  default="10.0.0.0/16"
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
 }
 
-variable "tenancy"
-{
-  default="dedicated"
+variable "tenancy" {
+  default = "dedicated"
 }
 
 variable "vpc_id" {}
 
-variable "subnet_id"
-{
-  default="10.0.0.0/24"
+variable "subnet_cidr" {
+  default = "10.0.1.0/24"
 }
