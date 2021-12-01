4f726a28db10b6904687c4514bacf5ffdf8926e1
SankajRam <44740469+SankajRam@users.noreply.github.com>
2021-12-01 21:24:39 +0530

    Update main.tf

--- eg-cloud-network/Dev/main.tf
+++ eg-cloud-network/Dev/main.tf
@@ -18,7 +18,7 @@ module "eg_vpc" {
 source = "../modules/eg-infra-vpc"
 vpc_cidr ="196.168.0.0/16"
 tenancy  ="default"
-vpc_id="${module.eg_vpc.vpc_id"
+vpc_id="${module.eg_vpc.vpc_id}"
 subnet_cidr="196.168.1.0/24"
   
 }
