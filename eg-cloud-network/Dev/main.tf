045a30e75255c55e8d6c6cc1116f12ab46a83312
SankajRam <44740469+SankajRam@users.noreply.github.com>
2021-12-01 20:36:44 +0530

    Update main.tf

--- eg-cloud-network/Dev/main.tf
+++ eg-cloud-network/Dev/main.tf
@@ -1,3 +1,19 @@
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = var.aws_region
+}
+
+
 module "eg_vpc" {
 source = "../modules/eg-infra-vpc"
 vpc_cidr ="196.168.0.0/16"
