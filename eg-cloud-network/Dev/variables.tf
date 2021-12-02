8fe51d135c64e10f5140270c67aa12269934871a
SankajRam <44740469+SankajRam@users.noreply.github.com>
2021-12-02 23:34:41 +0530

    Update variables.tf

--- eg-cloud-network/Dev/variables.tf
+++ eg-cloud-network/Dev/variables.tf
@@ -1,1 +1,19 @@
+variable "region"
+{
+  type        = string
+  description = "region"
+}
+
+variable "vpc_cidr"
+{
+  type        = string
+  description = "CIDR"
+}
+
+variable "subnet_cidr"
+{
+  type        = string
+  description = "CIDR"
+}
+
 
