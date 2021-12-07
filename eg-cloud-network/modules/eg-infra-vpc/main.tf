552ff744f267f1647665af1487036a3bfe12982a
SankajRam <44740469+SankajRam@users.noreply.github.com>
2021-12-07 22:06:12 +0530

    Update main.tf

--- eg-cloud-network/modules/eg-infra-vpc/main.tf
+++ eg-cloud-network/modules/eg-infra-vpc/main.tf
@@ -10,6 +10,11 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "${var.subnet_cidr}"
+  azs      = "${var.zone}"                                       //["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+private_subnets ="null"                                               //["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+public_subnets  = "null"                                              //["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+enable_nat_gateway = true
+enable_vpn_gateway = true
 
   tags = {
     Name = "Main"
