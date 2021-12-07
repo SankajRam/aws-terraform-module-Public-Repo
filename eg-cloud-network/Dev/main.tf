3b0679b6e60ba8a376eaa87bb0955ac0a121c24a
SankajRam <44740469+SankajRam@users.noreply.github.com>
2021-12-07 14:27:03 +0530

    Update main.tf

--- eg-cloud-network/Dev/main.tf
+++ eg-cloud-network/Dev/main.tf
@@ -18,9 +18,9 @@ module "eg_vpc" {
 source = "../modules/eg-infra-vpc"
 vpc_cidr ="${var.vpc_cidr}"
 tenancy  ="default"
-azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+azs      = "${var.zone}"                                       //["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+private_subnets ="null"                                               //["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+public_subnets  = "null"                                              //["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 enable_nat_gateway = true
 enable_vpn_gateway = true
 tags = {
