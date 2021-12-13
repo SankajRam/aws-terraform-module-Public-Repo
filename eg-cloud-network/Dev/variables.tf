ae4a6f393658db37d21cf2b7121f7bd1502df3b9
SankajRam <44740469+SankajRam@users.noreply.github.com>
2021-12-13 18:52:10 +0530

    Update variables.tf

--- eg-cloud-network/Dev/variables.tf
+++ eg-cloud-network/Dev/variables.tf
@@ -22,3 +22,21 @@ variable "vpc_name"{
   type   = string
   description= "vpc name "
 }
+
+variable "subnet" {
+   type = map
+   default = {
+      sub-1 = {
+         az = "az1"
+         cidr = "az1_cidr"
+      }
+      sub-2 = {
+         az = "az2"
+         cidr = "az2_cidr"
+      }
+      sub-3 = {
+         az = "az3"
+         cidr = "az3_cidr"
+      }
+   }
+}
