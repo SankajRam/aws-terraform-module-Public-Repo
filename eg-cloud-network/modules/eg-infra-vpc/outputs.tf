eb7727b4f8f5e1ff4cc52e49d44bdf083cd145ab
SankajRam <44740469+SankajRam@users.noreply.github.com>
2021-12-15 16:46:58 +0530

    Update outputs.tf

--- eg-cloud-network/modules/eg-infra-vpc/outputs.tf
+++ eg-cloud-network/modules/eg-infra-vpc/outputs.tf
@@ -2,7 +2,10 @@ output "aws_vpc"{
   value="${aws_vpc.r21vpc.id}"
 }
 
-output "subnet_id" {
-  value = "${aws_subnet.r21subnet.id}"
-}
+# output "subnet_id" {
+#   value = "${aws_subnet.r21subnet.id}"
+# }
 
+output "subnet_id" {
+aws_subnet.r21subnet[each.key]
+}
