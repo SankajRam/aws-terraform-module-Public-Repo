66de00ed9b1752c1856d06ed560ff9d3fa2c99d6
SankajRam <44740469+SankajRam@users.noreply.github.com>
2021-12-15 13:48:33 +0530

    Update variables.tf

--- eg-cloud-network/Dev/variables.tf
+++ eg-cloud-network/Dev/variables.tf
@@ -3,20 +3,20 @@ variable "region"{
   description = "region"
 }
 
-variable "zone"{
-  type        = string
-  description = "VPC Zone"
-}
+# variable "zone"{
+#   type        = string
+#   description = "VPC Zone"
+# }
 
 variable "vpc_cidr"{
   type        = string
   description = "CIDR"
 }
 
-variable "subnet_cidr"{
-  type        = string
-  description = "CIDR"
-}
+# variable "subnet_cidr"{
+#   type        = string
+#   description = "CIDR"
+# }
 
 variable "vpc_name"{
   type   = string
